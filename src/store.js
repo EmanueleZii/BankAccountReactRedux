9f385cfa3ec16a8574ea69561f4adca44ca993e0
unknown <zignale92@gmail.com>
2025-03-14 10:59:14 +0100

    install redux create store.js

--- src/store.js
+++ src/store.js
@@ -1,23 +1,72 @@
-const initialState ={
-    balance:0,
-    loan:0,
+import { createStore } from 'redux';
+
+const initialState = {
+    balance: 0,
+    loan: 0,
     loanPurpose: "",
 };
 
 function reducer(state = initialState, action) {
-    switch(action.type) {
+    switch (action.type) {
         case 'account/deposit':
-            return {...state, balance: state.balance + action.payload };
+            return { ...state, balance: state.balance + action.payload };
+
         case 'account/withdraw':
-            return {...state, balance: state.balance + action.payload };
+            return { ...state, balance: state.balance - action.payload };
+
         case 'account/requestLoan':
-            if (state.loan>0) return state;
-            //LAter
-            return {...state, loan: action.payload, loanPurpose: action.payload.purpose};
+            if (state.loan > 0) return state;
+            return {
+                ...state,
+                loan: action.payload.amount,
+                loanPurpose: action.payload.purpose,
+                balance: state.balance + action.payload.amount,
+            };
+
         case "account/payLoan":
-            return{...state, loan:0, loanPurpose:'', balance: state.balance - state.loan};
-            
+            if (state.loan === 0) return state;
+            return {
+                ...state,
+                balance: state.balance - state.loan,
+                loan: 0,
+                loanPurpose: ''
+            };
+
         default:
-            throw state;
+            return state;
     }
 }
+
+const store = createStore(reducer);
+
+// store.dispatch({ type: 'account/deposit', payload: 500 });
+// store.dispatch({ type: 'account/withdraw', payload: 200 });
+// store.dispatch({ type: 'account/requestLoan', payload: { amount: 1000, purpose: "Buy a car" } });
+// store.dispatch({ type: 'account/payLoan' });
+
+// store.dispatch({
+//     type: 'account/payLoan',
+// });
+
+const ACCOUNT_DEPOSIT = 'account/deposit';
+
+function deposit(amount) {
+    return { type: 'account/deposit', payload: amount };
+}
+
+function withdraw(amount) {
+    return{ type: 'account/withdraw', payload: amount }
+}
+
+function requestLoan(amount) {
+    return{ type: 'account/requestLoan', payload: { amount: amount, purpose: "Buy a car" } }
+}
+
+function payLoan() {
+    return { type: 'account/payLoan' };
+}
+
+store.dispatch(deposit(500));
+store.dispatch(withdraw(200));
+store.dispatch(requestLoan(1000));
+store.dispatch(payLoan());
